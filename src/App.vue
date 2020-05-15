56dc091d7e9d69592f89e4715f72f61f919afa00
rroossyyiidd <rroossyyiidd@gmail.com>
2020-05-15 14:48:34 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,91 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="root">
+    <div :class="cssClasses">
+      <router-view
+        name="layout"
+        :title="title"
+        :is-x-small="screen.isXSmall"
+        :is-large="screen.isLarge"
+      >
+        <div class="content">
+          <router-view name="content" />
+        </div>
+        <template #footer>
+          <the-footer />
+        </template>
+      </router-view>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheFooter from "./components/the-footer";
+import { sizes, subscribe, unsubscribe } from "./utils/media-query";
+
+function getScreenSizeInfo() {
+  const screenSizes = sizes();
+
+  return {
+    isXSmall: screenSizes["screen-x-small"],
+    isLarge: screenSizes["screen-large"],
+    cssClasses: Object.keys(screenSizes).filter(cl => screenSizes[cl])
+  };
+}
 
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      title: this.$appInfo.title,
+      screen: getScreenSizeInfo()
+    };
+  },
+  computed: {
+    cssClasses() {
+      return ["app"].concat(this.screen.cssClasses);
+    }
+  },
+  methods: {
+    screenSizeChanged() {
+      this.screen = getScreenSizeInfo();
+    }
+  },
+
+  mounted() {
+    subscribe(this.screenSizeChanged);
+  },
+
+  beforeDestroy() {
+    unsubscribe(this.screenSizeChanged);
+  },
+
   components: {
-    HelloWorld
+    TheFooter
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+html,
+body {
+  margin: 0px;
+  min-height: 100%;
+  height: 100%;
+}
+
+#root {
+  height: 100%;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+.app {
+  @import "./themes/generated/variables.base.scss";
+  background-color: darken($base-bg, 5);
+  display: flex;
+  height: 100%;
+  width: 100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import 'devextreme/dist/css/dx.common.css';
+import './themes/generated/theme.base.css';
+import './themes/generated/theme.additional.css';
+import Vue from "vue";
 
-Vue.config.productionTip = false
+import App from "./App";
+import router from "./router";
+import appInfo from "./app-info";
+
+Vue.config.productionTip = false;
+Vue.prototype.$appInfo = appInfo;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount("#app");
